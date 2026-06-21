6defa112f27f8f1d5817267ad0f698c0e7871e48
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-06-21 17:37:29 +0530

    refactor: change button width

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from "react";
 
 interface IButton {
     children?: ReactNode;
-    handleClick?(): void
+    handleClick?(): void;
+    fullWidth?: boolean;
 }
 
-export default function Button({ children, handleClick }: IButton) {
+export default function Button({ children, handleClick, fullWidth }: IButton) {
     return (
-        <button className="w-full text-white bg-emerald-600 hover:bg-emerald-700 rounded-lg py-2 px-4 cursor-pointer duration-100" onClick={handleClick || undefined}>{children}</button>
+        <button className={` text-white bg-emerald-600 hover:bg-emerald-700 rounded-lg py-2 px-4 cursor-pointer duration-100 ${fullWidth && 'w-full'}`} onClick={handleClick || undefined}>{children}</button>
     )
 }
