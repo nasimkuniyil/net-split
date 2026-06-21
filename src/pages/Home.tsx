b2594eb80a0b28d8053db6b162f8e4edfa35e7f4
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-06-21 21:48:57 +0530

    refactor: remove unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import Tile from "../components/Tile";
-import { IndianRupee } from "lucide-react";
 import ActionLink from "../components/ActionLink";
 import StatCard from "../components/StatCard";
 
