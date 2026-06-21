a403a05a548c2d0d450af6608cf1ee479a112e91
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-06-21 21:48:15 +0530

    feat: implement Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
+import Tile from "../components/Tile";
+import { IndianRupee } from "lucide-react";
+import ActionLink from "../components/ActionLink";
+import StatCard from "../components/StatCard";
+
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+            <div className="text-end mb-5">
+                <Button>Create New Book</Button>
+            </div>
+            <div>
+                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+
+                    <ActionLink href={`/expense-books/`}>
+                        <Tile title={"Weekend Ride"}>
+                            <StatCard amount={2500} />
+                        </Tile>
+                    </ActionLink>
+                    <ActionLink href={`/expense-books/`}>
+                        <Tile title={"Hostel Food"}>
+                            <StatCard amount={1750} />
+                        </Tile>
+                    </ActionLink>
+                    <ActionLink href={`/expense-books/`}>
+                        <Tile title={"Goa Trip"}>
+                            <StatCard amount={5000} />
+                        </Tile>
+                    </ActionLink>
+
+                </ul>
+            </div>
+        </div>
+    )
 }
