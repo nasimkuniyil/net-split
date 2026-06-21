864fa8b79fbf96f18d719a7892ddad44e384834c
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-06-21 17:20:02 +0530

    refactor: update style

--- src/components/ActionLink.tsx
+++ src/components/ActionLink.tsx
@@ -7,11 +7,11 @@ interface IActionLink {
 }
 
 const ACTION_LINK_STYLE = {
-    fill: "text-white bg-emerald-600 hover:bg-emerald-700 rounded-lg py-2 px-4 cursor-pointer duration-100"
+    fill: "text-white bg-emerald-600 hover:bg-emerald-700 rounded-lg py-2 px-4"
 }
 
 export default function ActionLink({ children, href, fill }: IActionLink) {
     return (
-        <a href={href} className={`py-2 px-4 text-neutral-500 cursor-pointer duration-100 ${fill ? ACTION_LINK_STYLE.fill : "hover:underline"}`} target='_blank'>{children}</a>
+        <a href={href} className={`cursor-pointer duration-100 ${fill ? ACTION_LINK_STYLE.fill : "hover:underline text-neutral-500 "}`} target='_blank'>{children}</a>
     )
 }
