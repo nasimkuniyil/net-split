2c59fdb9f2bace3f751d4b0151d38acb362b09d6
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-06-21 16:09:23 +0530

    feat: add Tile component

---
+++ src/components/Tile.tsx
@@ -0,0 +1,17 @@
+import { type ReactNode } from "react"
+
+interface ITail {
+    children: ReactNode;
+    title?: string;
+}
+
+export default function Tail({ children, title }: ITail) {
+    return (
+        <div className="p-6 bg-white max-w-3xl border border-neutral-200 shadow-xs rounded-lg">
+            <div className="mb-3">
+                <h3 className="md:text-lg font-bold">{title || "Title"}</h3>
+            </div>
+            {children}
+        </div>
+    )
+}
